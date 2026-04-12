47e7e5339be7b7d78657f8801b5665b3728ac823
KaiHikichi <kai.hikichi@gmail.com>
2026-04-11 23:17:59 -0700

    updated main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from util.search import A_Star
 from util.graph import Weather
 from util.graph import simulateConstruction
 from util.edge_operations import METERS_PER_MIN
+from util.edge_operations import update_edge_costs_in_path, estimate_time_per_edge
 
 METERS_PER_DEGREE = 111139 
 
@@ -17,21 +18,35 @@ def main():
     map: NodeMap = NodeMap()
     setUp(map, "maps/mapNodes.json")
 
-    #update based on wether
-    map.updateWeatherCosts(0.5, Weather.CLEAR)
+    while True:
 
-    #simulate construction
-    simulateConstruction(map, 0.001, 2)
+        #update based on wether
+        map.updateWeatherCosts(0.5, Weather.CLEAR)
 
-    #find path
-    #(path, cost) = A_Star(map.nodes["Residence Townhouses"], map.nodes["CS Common Room"])
-    (path, cost) = A_Star(map.nodes["ASB Entrance"], map.nodes["CS Common Room"])
+        #simulate construction
+        simulateConstruction(map, 0.001, 2)
 
+        #get start and dest
+        start = input("Start: ")
+        dest = input("Dest: ")
 
-    #print the path
-    for node in path:
-        print(node)
-    print(cost * METERS_PER_DEGREE / METERS_PER_MIN)
+        start = "ASB Entrance"
+        dest = "CS Common Room"
+
+        #find path
+        #(path, cost) = A_Star(map.nodes["Residence Townhouses"], map.nodes["CS Common Room"])
+        (path, cost) = A_Star(map.nodes[start], map.nodes[dest])
+
+        #print the path
+        for node in path:
+            print(node)
+        print(cost * METERS_PER_DEGREE / METERS_PER_MIN)
+
+        #get how long it took for user to walk path
+        #update the edge costs to learn different walking speeds
+        time = input("How long did it take to walk in minutes: ")
+        times = estimate_time_per_edge(path, int(time), cost)
+        update_edge_costs_in_path(path, times)
 
 
     pass
